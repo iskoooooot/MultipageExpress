7644bd9caaee1b9bb51183601acbd8e4553691dd
iskut <mamhutscott23@gmail.com>
2025-03-20 18:38:44 +0800

    `Added dynamic blog page serving and fs module import`

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,7 +21,20 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
+// Serve blog page dynamically
+app.get('/blog', (req, res) => {
+    const postsPath = path.join(__dirname, 'data', 'posts.json');
+    fs.readFile(postsPath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading blog posts');
+            return;
+        }
+        const posts = JSON.parse(data);
+        res.render('blog', { posts });
+    });
+});
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
